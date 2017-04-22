72b4463a1a97ac11f7ec1383bcf5c12d72123c95
firen777 <freeze1995831@yahoo.com>
2017-04-22 02:14:01 -0700

    first commit

---
+++ src/Tester.java
@@ -0,0 +1,48 @@
+import processing.core.PApplet;
+
+/**
+ * 
+ */
+
+/**
+ * @author Albert
+ *
+ */
+public class Tester extends PApplet{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PApplet.main("Tester");
+
+	}
+
+	/* (non-Javadoc)
+	 * @see processing.core.PApplet#draw()
+	 */
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		super.draw();
+	}
+
+	/* (non-Javadoc)
+	 * @see processing.core.PApplet#settings()
+	 */
+	@Override
+	public void settings() {
+		// TODO Auto-generated method stub
+		super.settings();
+	}
+
+	/* (non-Javadoc)
+	 * @see processing.core.PApplet#setup()
+	 */
+	@Override
+	public void setup() {
+		// TODO Auto-generated method stub
+		super.setup();
+	}
+
+}
